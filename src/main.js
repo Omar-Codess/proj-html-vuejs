3e567b4a93a486d2636996e42d4cc6b2e46056bd
Omar-Codess <omartaki8@gmail.com>
2022-11-18 11:10:43 +0100

    Installato Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './style.css'
 import App from './App.vue'
 
