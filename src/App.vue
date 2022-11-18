0e2e69042c23744b3ecdfd919d1bf620a633bf78
Omar-Codess <omartaki8@gmail.com>
2022-11-18 18:35:04 +0100

    creato banner in alto

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,33 @@
 </script>
 
 <template>
-    
+  <div class="banner">
+    <p>Everything about Lifestyle, Travel and Gadgets!</p>
+    <span>SIGN IN</span>
+    <span>ABOUT US</span>
+    <span>CONTACT US</span>
+    <span>BUY NOW</span>
+  </div>
+  
 </template>
 
 <style>
+  .banner{
+    background-color: #0088cc;
+    color: white;
+    display: flex;
+    min-width: 1140px;
+    align-items: center;
+    height: 40px;
+    font-weight: 300;
+  }
 
+  .banner p{
+    margin-right: 2em;
+    margin-top: 1em;
+  }
+
+  .banner span{
+    margin-right: 1em;
+  }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,13 @@
 <script>
+export default {
+    name: "AppHeader"
+}
 </script>
 
 <template>
+    <div class="container border-dark">
+
+    </div>
 </template>
 
 <style>
