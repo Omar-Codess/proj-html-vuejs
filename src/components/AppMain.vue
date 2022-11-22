59c8f62eb6fb73457eb4c52a7f5e522cc4270d2a
Omar-Codess <omartaki8@gmail.com>
2022-11-22 10:38:20 +0100

    Completate prime 4 cards del main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,8 +20,116 @@ export default {
             </div>
         </div>
     </div>
-<!-- /Categorie -->
+    <!-- /Categorie -->
 
+    <!-- Main -->
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="left-img">
+                    <img src="../assets/img/blog-54.jpg" alt="">
+                </div>
+
+                <div class="left-text">
+                    <div class="date-icon">
+                        <p class="number">12</p>
+                        <p class="month">JAN</p>
+                    </div>
+                    <div class="mt-3 ms-3">
+                        <h4>How to Make Friends as a Grown-Up</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta officiis ex placeat ratione autem debitis eveniet, nulla, earum fuga ipsum qui at odit laborum alias, non eligendi dolores nesciunt.
+                        Suscipit cupiditate error eaque quisquam vitae pariatur, voluptatem in iste distinctio recusandae labore unde temporibus repellendus ipsum minus alias magni voluptatum, praesentium assumenda quas consectetur asperiores molestias quod! Officia, esse. [...]</p>
+                        <div class="left-text-footer">
+                            <p><i class="fa-regular fa-user"></i> <a href="">By John Doe</a> <i class="fa-regular fa-folder"></i> <a href="">Lifestyle</a>, <a href="">Travel</a> <i class="fa-regular fa-comments"></i> <a href="">12 comments</a></p>
+                            <span class="read-more-btn">READ MORE</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="left-img mt-5">
+                    <img src="../assets/img/blog-55 (1).jpg" alt="">
+                </div>
+                <div class="left-text">
+                    <div class="date-icon">
+                        <p class="number">12</p>
+                        <p class="month">JAN</p>
+                    </div>
+                    <div class="mt-3 ms-3">
+                        <h4>Simple Ways to Have a Pretty Face</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta officiis ex placeat ratione autem debitis eveniet, nulla, earum fuga ipsum qui at odit laborum alias, non eligendi dolores nesciunt.
+                        Suscipit cupiditate error eaque quisquam vitae pariatur, voluptatem in iste distinctio recusandae labore unde temporibus repellendus ipsum minus alias magni voluptatum, praesentium assumenda quas consectetur asperiores molestias quod! Officia, esse. [...]</p>
+                        <div class="left-text-footer">
+                            <p><i class="fa-regular fa-user"></i> <a href="">By John Doe</a> <i class="fa-regular fa-folder"></i> <a href="">Photography</a>, <a href="">Travel</a> <i class="fa-regular fa-comments"></i> <a href="">12 comments</a></p>
+                            <span class="read-more-btn">READ MORE</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="left-img mt-5">
+                    <img src="../assets/img/blog-56 (1).jpg" alt="">
+                </div>
+                <div class="left-text">
+                    <div class="date-icon">
+                        <p class="number">12</p>
+                        <p class="month">JAN</p>
+                    </div>
+                    <div class="mt-3 ms-3">
+                        <h4>Ranking the greatest players in basketball</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta officiis ex placeat ratione autem debitis eveniet, nulla, earum fuga ipsum qui at odit laborum alias, non eligendi dolores nesciunt.
+                        Suscipit cupiditate error eaque quisquam vitae pariatur, voluptatem in iste distinctio recusandae labore unde temporibus repellendus ipsum minus alias magni voluptatum, praesentium assumenda quas consectetur asperiores molestias quod! Officia, esse. [...]</p>
+                        <div class="left-text-footer">
+                            <p><i class="fa-regular fa-user"></i> <a href="">By John Doe</a> <i class="fa-regular fa-folder"></i> <a href="">Sports</a>, <a href="">Business</a> <i class="fa-regular fa-comments"></i> <a href="">12 comments</a></p>
+                            <span class="read-more-btn">READ MORE</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="left-img mt-5">
+                    <img src="../assets/img/blog-58 (1).jpg" alt="">
+                </div>
+                <div class="left-text">
+                    <div class="date-icon">
+                        <p class="number">12</p>
+                        <p class="month">JAN</p>
+                    </div>
+                    <div class="mt-3 ms-3">
+                        <h4>Top Camper Trailer Towing Tips</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta officiis ex placeat ratione autem debitis eveniet, nulla, earum fuga ipsum qui at odit laborum alias, non eligendi dolores nesciunt.
+                        Suscipit cupiditate error eaque quisquam vitae pariatur, voluptatem in iste distinctio recusandae labore unde temporibus repellendus ipsum minus alias magni voluptatum, praesentium assumenda quas consectetur asperiores molestias quod! Officia, esse. [...]</p>
+                        <div class="left-text-footer">
+                            <p><i class="fa-regular fa-user"></i> <a href="">By John Doe</a> <i class="fa-regular fa-folder"></i> <a href="">Travel</a>, <a href="">Lifestyle</a> <i class="fa-regular fa-comments"></i> <a href="">12 comments</a></p>
+                            <span class="read-more-btn">READ MORE</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid-six-images mt-5">
+                    <img src="../assets/img/blog-58 (1).jpg" alt="">
+                </div>
+                <div class="left-text">
+                    <div class="date-icon">
+                        <p class="number">8</p>
+                        <p class="month">JAN</p>
+                    </div>
+                    <div class="mt-3 ms-3">
+                        <h4>10 Best Travel Tips After 5 Years Travelling The World</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta officiis ex placeat ratione autem debitis eveniet, nulla, earum fuga ipsum qui at odit laborum alias, non eligendi dolores nesciunt.
+                        Suscipit cupiditate error eaque quisquam vitae pariatur, voluptatem in iste distinctio recusandae labore unde temporibus repellendus ipsum minus alias magni voluptatum, praesentium assumenda quas consectetur asperiores molestias quod! Officia, esse. [...]</p>
+                        <div class="left-text-footer">
+                            <p><i class="fa-regular fa-user"></i> <a href="">By John Doe</a> <i class="fa-regular fa-folder"></i> <a href="">Travel</a>, <a href="">Lifestyle</a> <i class="fa-regular fa-comments"></i> <a href="">12 comments</a></p>
+                            <span class="read-more-btn">READ MORE</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-3 border border-dark">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nostrum accusantium repellat quidem maxime saepe, tempore officia laudantium facilis minus quis ullam, excepturi explicabo consequuntur autem totam dicta? Obcaecati, accusantium!
+                Consectetur ullam dolore, qui accusamus, recusandae eligendi est dolor deserunt incidunt adipisci suscipit natus, debitis rerum autem consequuntur tempora accusantium! Amet obcaecati nihil cum deserunt explicabo perspiciatis quis quas et?
+            </div>
+        </div>
+    </div>
+    <!-- /Main -->
 </template>
 
 <style>
@@ -50,5 +158,63 @@ export default {
     position: absolute;
     right: 185px;
     bottom: -570px;
+}
+
+/* MAIN */
+.left-img{
+    height: 400px;
+}
+
+.left-img img{
+    max-width: 100%;
+}
+
+.left-text{
+    display: flex;
+}
+/* Date icon */
+.date-icon{
+    margin-top: 1rem;
+    height: 80px;
+    width: 10%;
+    font-weight: 600;
+    text-align: center;
+}
+
+.number{
+    height: 60%;
+    background-color: #f7f7f7;
+    vertical-align: middle;
+    line-height: 50px;
+    margin-bottom: 0px;
+}
+
+.month{
+    background-color: #212529;
+    color: white;
+    font-size: 12px;
+}
+
+/* Left text footer */
+
+.left-text-footer{
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #e2e2e2;
+    min-height: 80px;
+}
+
+a{
+    text-decoration: none;
+}
+
+.read-more-btn{
+    border: 1px solid #e2e2e2;
+    font-size: 14px;
+    height: 44px;
+    border-radius: 8px;
+    color: #888888;
+    line-height: 40px;
+    vertical-align: middle;
 }
 </style>
